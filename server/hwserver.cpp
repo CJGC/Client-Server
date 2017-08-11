ed9e5654710f4739595636fa1fd9cc210efca93f
cjgc <chjugomez@utp.edu.co>
2017-08-10 21:58:37 -0500

    hwserver.cpp file was modified to first stage

--- server/hwserver.cpp
+++ server/hwserver.cpp
@@ -1,43 +1,48 @@
 #include <zmqpp/zmqpp.hpp>
 #include <iostream>
+#include <vector>
 #include <string>
 #include <cassert>
+#include "header.h"
 
-int main() {
-  /* making socket and contex */
-  zmqpp::context ctx;
-  zmqpp::socket s(ctx, zmqpp::socket_type::rep);
-
-  /* binding socket with given tcp port */
-  std::cout << "Binding socket to tcp port 5555\n";
-  s.bind("tcp://*:5555");
-  std::cout << "Start serving requests!\n";
-
+void listen(zmqpp::socket &s){
   while(true) {
-    /* building package that will contain client message */
-    zmqpp::message m;
-    s.receive(m);
-
-    /* each message has three parts: <operation|operand1|operand2> */
-    assert(m.parts() == 3);
-
-    /* processing package */
+    zmqpp::message requestPack, response;
     std::string operation;
     int operand1, operand2, answer = 0;
+    s.receive(requestPack);
 
-    m >> operation >> operand1 >> operand2;
-    std::cout << "Working on  " << operation << std::endl;
+    /* each message has three parts: <operation|operand1|operand2> */
+    assert(requestPack.parts() == 3);
+
+    requestPack >> operation >> operand1 >> operand2;
+    std::cout << "Working on " << operation << std::endl;
     if(operation == "add") answer = operand1 + operand2;
     else if(operation == "sub") answer = operand1 - operand2;
     else std::cout << "Invalid operation requested!!\n";
 
     /* replying to client */
     std::cout << "Sending response  " << answer << std::endl;
-    zmqpp::message response;
     response << answer;
     s.send(response);
   }
+}
 
-  std::cout << "Finished\n";
+int main() {
+  /* making socket and contex */
+  zmqpp::context ctx;
+  zmqpp::socket s(ctx, zmqpp::socket_type::rep);
+
+  /* storing files names with desired format */
+  std::vector<const char *> files;
+  getFiles(files,".cpp");
+
+  /* binding socket with given tcp port */
+  std::cout << "Binding socket to tcp port 5555\n";
+  s.bind("tcp://*:5555");
+  std::cout << "Start serving requests!\n";
+
+  /* listening client's requests */
+  listen(s);
   return 0;
 }
