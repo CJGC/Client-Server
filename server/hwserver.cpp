131506db89dc0ac83de26a6369e822bdc4a15188
cjgc <chjugomez@utp.edu.co>
2017-08-19 23:43:55 -0500

    hwserver V1, this server allows list available songs, download a requested song per parts (512 kb)

--- server/hwserver.cpp
+++ server/hwserver.cpp
@@ -1,82 +1,61 @@
 #include <zmqpp/zmqpp.hpp>
 #include "header.h"
 
-void listen(zmqpp::socket& s,std::string& songsPackage,std::unordered_map<std::string,std::vector<char>>& songs){
-  std::unordered_map<std::string,std::vector<size_t>> clients;
-  size_t sizePackage=0, i=0, songParts=0;
-  zmqpp::message clientRequest, reply;
-  std::string id, op, songName;
-  //std::vector<char> package;
+using namespace zmqpp;
+using namespace std;
+
+void listen(socket& s,string& songsPackage,string& musPath){
+  message clientRequest, reply;
+  unsigned int part = 0;
+  string op, songName;
   while(true){
     s.receive(clientRequest);
-    // std::cout<< clientRequest.parts();
-    assert(clientRequest.parts() == 3);
-    clientRequest >> id >> op >> songName;
+    // assert(clientRequest.parts() == 3);
+    clientRequest >> op >> songName >> part;
     if(op == "list") reply << songsPackage;
-    else if(op == "play")
-      if(songs.find(songName) != songs.end()){
-        if(clients.find(id) != clients.end()){ //client exist
-          i = clients[id].at(0);
-          songParts = clients[id].at(1);
-          std::vector<char> package = getPackage(songs[songName],i,512000,sizePackage);
-          std::cout<<sizePackage<<"\n";
-          songParts--;
-          reply << songParts;
-          reply.add_raw(package.data(),sizePackage);
-          if(songParts != 0){clients[id].at(0) = i; clients[id].at(1) = songParts;}
-          else clients.erase(id);
-        }
-        else{ // client doesn't exist
-          double parts = double(songs[songName].size()) / double(512000);
-          i=0;
-          songParts = std::ceil(parts);
-          std::vector<char> package = getPackage(songs[songName],i,512000,sizePackage);
-          if(songParts > 1){
-            songParts--;
-            std::vector<size_t> clientInfo(2); clientInfo.at(0) = i; clientInfo.at(1) = songParts;
-            clients[id] = clientInfo;
-          }
-          reply << songParts;
-          reply.add_raw(package.data(),sizePackage);
-        }
-      }
-      else reply << "Desired song could not be found!";
+    else if(op == "getPart"){
+      size_t sizePackage = 0;
+      vector<char> package = getPackage(musPath,songName,512000,part,sizePackage);
+      reply.add_raw(package.data(),sizePackage);
+    }
+    else if(op == "meetParts") reply << meetParts(musPath,songName,512000);
     else reply << "Invalid operation requested!\n";
     s.send(reply);
   }
+
 }
 
 int main(int argc,const char *argv[]){
   if(argc != 2){
-    std::cerr <<"There should be two arguments: <"<< argv[0]<<"> <dir path/>\n";
+    cerr <<"There should be two arguments: <"<< argv[0]<<"> <dir path/>\n";
     return -1;
   }
-  std::string path = argv[1];
+  string path = argv[1];
 
   /* making socket and contex */
-  zmqpp::context ctx;
-  zmqpp::socket s(ctx, zmqpp::socket_type::rep);
+  context ctx;
+  socket s(ctx, socket_type::rep);
 
   /* getting songs filename */
-  std::vector<std::string> files;
+  vector<string> files;
   if(getFiles(files,".ogg",path.c_str()) != 0){
-    std::cerr<<"Was not possible open <"<<path<<">!";
+    cerr<<"Was not possible open <"<<path<<">!";
     return -1;
   }
 
   /* building songs package + unordered_map that will contain songs */
-  std::string songsPackage("");
-  std::unordered_map<std::string,std::vector<char>> songs;
-  for(std::string song:files){
+  string songsPackage("");
+  // unordered_map<string,vector<char>> songs;
+  for(string song:files){
     songsPackage+=song+"\n";
-    songs[song] = readFileToBytes(path+song);
+    // songs[song] = readFileToBytes(path+song);
   }
 
   /* binding socket with given tcp port */
-  std::cout << "Binding socket to tcp port 5555\n";
+  cout << "Binding socket to tcp port 5555\n";
   s.bind("tcp://*:5555");
-  std::cout << "Start serving requests!\n";
+  cout << "Start serving requests!\n";
   /* listening client's requests */
-  listen(s,songsPackage,songs);
+  listen(s,songsPackage,path);
   return 0;
 }
