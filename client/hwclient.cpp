5ef503926ff433bd069c9df0a83e1c9a8c037e15
cjgc <chjugomez@utp.edu.co>
2017-08-25 00:31:03 -0500

    The code was commented, some adjustments were made

--- client/hwclient.cpp
+++ client/hwclient.cpp
@@ -8,12 +8,12 @@
 #include <mutex>
 #include <condition_variable>
 #include <string>
-#include <cstdlib>
 
 using namespace std;
 using namespace zmqpp;
 using namespace sf;
 
+/* needed global variables (these variables must be seen by all members)*/
 list<string> userList;
 list<string>::iterator currentSongItera= userList.begin();
 Music music;
@@ -22,6 +22,7 @@ condition_variable cv;
 bool isPlayedByUser = false;
 
 void autoPlay(){
+	/* it will put a thread into autoplay action respecting certain rules */
 	while(true){
 		unique_lock<mutex> lock(autPlay);
 		cv.wait(lock,[]{return isPlayedByUser;});
@@ -35,6 +36,7 @@ void autoPlay(){
 }
 
 void getPart(socket& s,string songName,unsigned int part,ofstream &ofs){
+	/* it will get a given part of requested song */
 	message request,answer;
 	request << "getPart" << songName << part;
 	s.send(request);
@@ -46,8 +48,9 @@ void getPart(socket& s,string songName,unsigned int part,ofstream &ofs){
 }
 
 void getSong(socket &s,string& songName){
+	/* it will get song knowing the song's parts first, then requesting part by part */
 	message request,answer;
-	request <<"meetParts"<< songName << 0;
+	request << "meetParts" << songName << 0;
 	s.send(request);
 	s.receive(answer);
 	size_t parts=0;
@@ -58,6 +61,7 @@ void getSong(socket &s,string& songName){
 }
 
 list<string> processList(string& sList){
+	/* it will turn into a true object c++ list from a concatenated string sList */
 	list<string> songsList; string songName("");
 	for(auto c : sList){
 		if(c == '\n'){songsList.push_back(songName); songName = "";}
@@ -67,6 +71,7 @@ list<string> processList(string& sList){
 }
 
 list<string> getList(socket &s){
+	/* it will get the list of available songs from hwserver.cpp */
 	message request,answer;
 	string sList;
 	request << "list" << "none" << 0;
@@ -76,8 +81,10 @@ list<string> getList(socket &s){
 	return processList(sList);
 }
 
-void makeLine(string& item,string& screen,const size_t& screenSize,size_t rule){
-	size_t spaces = screenSize - item.size() - 2, makeRule = 0;
+void makeLine(string& line,string& screen,const size_t& screenSize,size_t rule){
+	/* it will make a line with a rule. A rule could be the number of spaces before
+	a line, like second rule in the example below (currently exist two rules) */
+	size_t spaces = screenSize - line.size() - 2, makeRule = 0;
 	if(rule == 1) makeRule = spaces/2;
 	if(rule == 2) makeRule = 7;
 	for(size_t p=1; p<=spaces+1; p++)	if(p == makeRule) screen += item; else screen += " ";
@@ -85,6 +92,7 @@ void makeLine(string& item,string& screen,const size_t& screenSize,size_t rule){
 }
 
 string menu(string title,list<string>& content,list<string>& availOpts){
+	/* it will print a menu, with menu title, menu content  and available options for menu */
 	string screen(""), option("");
 	string line("--------------------------------------------------------------------------------");
 	const size_t screenSize = line.size();
@@ -102,15 +110,18 @@ string menu(string title,list<string>& content,list<string>& availOpts){
 }
 
 bool intoList(list<string>& _list,string& songName){
+	/* it will check if song name is into given list */
 	if(find(_list.begin(),_list.end(),songName) != _list.end()) return true;
 	return false;
 }
 
 list<string>::iterator getIterator(string& songName){
+	/* it will return iterator pointer of the given song name */
 	return find(userList.begin(),userList.end(),songName);
 }
 
 void common(list<string>& content,list<string>& availOpts,string title,string& fpart,string& lpart){
+	/* common function is common code lines that can be fit into an unique function */
 	string userOpt("");
 	do{
 		userOpt = menu(title,content,availOpts);
@@ -128,27 +139,26 @@ int main(int argc, char **argv) {
 	s.connect("tcp://localhost:5555");
 
 	/* interacting with user */
-	bool exit = false;
 	int menuNum = 1;
 	list<string> serverMusicList = getList(s);
 	thread t(autoPlay);
 	do{
 		switch(menuNum){
-			case 1:{ // Available songs menu
-				list<string> availOpts{"add","goto playlist"};
-				string fpart(""), lpart("");
+			case 1:{ // available songs menu
+				list<string> availOpts{"add","goto playlist"}; // available options for first menu
+				string fpart(""), lpart("");									 // first part and last part of user's string
 				common(serverMusicList,availOpts,"AVAILABLE SONGS MENU",fpart,lpart);
 				if(fpart == "goto" && lpart == "playlist"){menuNum = 2; continue;}
-				string songName = lpart;
+				string songName = lpart;											 // if lpart isn't playlist it will probably be a songName
 				if(fpart == "add" && intoList(serverMusicList,songName) && !intoList(userList,songName)){
 					getSong(s,songName);
 					userList.push_back(songName);
 				}
 			}break;
-		 	case 2:{ // Playlist user menu
+		 	case 2:{ // playlist user menu
 				list<string> availOpts{"play","remove","stop song","next song","goto store"};
 				string fpart(""), lpart("");
-				common(userList,availOpts,"PLAY LIST MENU",fpart,lpart);
+				common(userList,availOpts,"PLAYLIST MENU",fpart,lpart);
 				if(fpart == "goto" && lpart == "store"){menuNum = 1; continue;}
 				if(fpart == "stop" && lpart == "song"){isPlayedByUser = false; music.stop();continue;}
 				if(fpart == "next" && lpart == "song" && !userList.empty() && isPlayedByUser){
@@ -173,10 +183,9 @@ int main(int argc, char **argv) {
 					int j = distance(userList.begin(),currentSongItera);
 					userList.remove(songName);
 					if(i == j){currentSongItera = userList.begin();isPlayedByUser=false; music.stop();}
-					else if(j > i) advance(currentSongItera,-1);
 				}
 			} break;
 		}
-	}while(!exit);
+	}while(true);
 	return 0;
 }
