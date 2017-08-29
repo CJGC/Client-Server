6788d8d26692f717aa524012ccef769f09b4f718
cjgc <chjugomez@utp.edu.co>
2017-08-28 21:30:00 -0500

    oop paradigm was introduced. Some variables were renamed. Server class was introduced

--- server/hwserver.cpp
+++ server/hwserver.cpp
@@ -4,26 +4,41 @@
 using namespace zmqpp;
 using namespace std;
 
-void listen(socket& s,string& songsPackage,string& musPath){
-  message clientRequest, reply;
-  unsigned int part = 0;
-  string op, songName;
-  while(true){
-    s.receive(clientRequest);
-    // assert(clientRequest.parts() == 3);
-    clientRequest >> op >> songName >> part;
-    if(op == "list") reply << songsPackage;
-    else if(op == "getPart"){
-      size_t sizePackage = 0;
-      vector<char> package = getPackage(musPath,songName,512000,part,sizePackage);
-      reply.add_raw(package.data(),sizePackage);
-    }
-    else if(op == "meetParts") reply << meetParts(musPath,songName,512000);
-    else reply << "Invalid operation requested!\n";
-    s.send(reply);
-  }
+class Server{
+  private:
+    message cliReq, reply;
+    unsigned int part;
+    string op, song, songsPackage, dir;
 
-}
+    void buildSongsPackage(vector<string>& songs){
+      /* it will build available songs package */
+      for(string& song: songs) songsPackage+=song+"\n";
+    }
+
+  public:
+    Server(string& dir){
+      vector<string> songs;
+      if(getSongs(songs,".ogg",dir.c_str()) != 0) exit(1);
+      buildSongsPackage(songs);
+      this->dir = dir;
+    }
+
+    void listen(socket& s){
+      /* it will listen client's request */
+      while(true){
+        s.receive(cliReq);
+        cliReq >> op >> song >> part;
+        if(op == "getSongPart"){
+          size_t partSize = 0;
+          vector<char> songPart = getSongPart(dir,song,512000,part,partSize);
+          reply.add_raw(songPart.data(),partSize);
+        }
+        else if(op == "list") reply << songsPackage;
+        else if(op == "songParts") reply << songParts(dir,song,512000);
+        s.send(reply);
+      }
+    }
+};
 
 int main(int argc,const char *argv[]){
   if(argc != 2){
@@ -31,31 +46,12 @@ int main(int argc,const char *argv[]){
     return -1;
   }
   string path = argv[1];
-
-  /* making socket and contex */
   context ctx;
   socket s(ctx, socket_type::rep);
-
-  /* getting songs filename */
-  vector<string> files;
-  if(getFiles(files,".ogg",path.c_str()) != 0){
-    cerr<<"Was not possible open <"<<path<<">!";
-    return -1;
-  }
-
-  /* building songs package + unordered_map that will contain songs */
-  string songsPackage("");
-  // unordered_map<string,vector<char>> songs;
-  for(string song:files){
-    songsPackage+=song+"\n";
-    // songs[song] = readFileToBytes(path+song);
-  }
-
-  /* binding socket with given tcp port */
+  Server server(path);
   cout << "Binding socket to tcp port 5555\n";
   s.bind("tcp://*:5555");
-  cout << "Start serving requests!\n";
-  /* listening client's requests */
-  listen(s,songsPackage,path);
+  cout << "listening...!\n";
+  server.listen(s);
   return 0;
 }
