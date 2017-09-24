b61e0a2ee535173fa29b75e5ba7dd83b2a2091a6
cjgc <chjugomez@utp.edu.co>
2017-09-24 15:32:38 -0500

    GraphReader.hh, it will load graph into memory

---
+++ matrices/graphReader.hh
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+vector<T> readGraph(string fileName){
+   vector<T> m;
+   ifstream infile(fileName);
+   string line;
+   unsigned int nodes;
+
+   while(getline(infile,line)){
+      istringstream iss(line);
+      if(line[0] == 'p'){
+         string s1, s2;
+         iss >> s1 >> s2 >> nodes; 
+         m.resize(nodes*nodes);
+         cout << "Graph with " << nodes << " nodes" << endl;
+      }
+      else if(line[0] == 'e'){
+         char e;
+         unsigned int i,j; 
+         iss >> e >> i >> j;
+         m[(i-1)*nodes + (j-1)] = 1;
+      }
+   }
+   return m;
+}
