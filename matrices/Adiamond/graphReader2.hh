d8ed96df2c767c38bac60d52710f7bced78e2883
cjgc <chjugomez@utp.edu.co>
2017-10-22 13:05:49 -0500

    Diagonal nodes are now considered

--- matrices/Adiamond/graphReader2.hh
+++ matrices/Adiamond/graphReader2.hh
@@ -2,12 +2,21 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
+#include "node.hh"
 
 using namespace std;
 typedef string str;
 
-void loadgraph(str fileName){
+void BuildDiagonal(vec* graph){
+  /* it will build i == j diagonal into graph */
+  uint nodes = graph[0].size();
+  for(uint n=0; n<nodes; n++){
+    node *newNode = buildNode(n,n,0);
+    linkNode(graph,newNode);
+  }
+}
+
+void loadgraph(str fileName,vec* graph){
   ifstream infile(fileName);
   str line;
 
@@ -17,14 +26,18 @@ void loadgraph(str fileName){
       str op, word;
       uint nodes,arch;
       iss >> op >> word >> nodes >> arch;
+      graph[0].resize(nodes); // row vector
+      graph[1].resize(nodes); // col vector
       cout << "Graph with " << nodes << " nodes" << endl;
     }
     else if(line[0] == 'e'){
       char op;
       uint i,j, weight = 1;
-      iss >> op >> i >> j; //>> weight;
+      iss >> op >> i >> j >> weight;
+      node *newNode = buildNode(i-1,j-1,weight);
+      linkNode(graph,newNode);
     }
   }
-
   infile.close();
+  BuildDiagonal(graph);
 }
