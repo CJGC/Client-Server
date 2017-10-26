0d70c456185bbc6451a1841e0f466b6984c59e6c
cjgc <chjugomez@utp.edu.co>
2017-10-26 17:56:24 -0500

    Code adjustments

--- matrices/Adiamond/devStand.hh
+++ matrices/Adiamond/devStand.hh
@@ -5,8 +5,7 @@ using namespace std;
 
 template <typename T> double arithMean(const vector<T> &vi) {
   double cont = 0;
-  for (const T &t : vi)
-    cont += t;
+  for (const T &t : vi) cont += t;
   return (cont / vi.size());
 }
 
@@ -14,9 +13,7 @@ template <typename T> double stdDev(const vector<T> &vi) {
   double mean = arithMean(vi);
   double cont = 0;
 
-  for (int i = 0; i < vi.size(); ++i) {
-    cont += pow((vi[i] - mean), 2);
-  }
+  for(const T &t : vi) cont += pow((t - mean), 2);
 
   return sqrt(cont / vi.size());
 }
