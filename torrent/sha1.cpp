1e2996f7c400f389da3f2b4a2a66e215fbabd5a1
cjgc <chjugomez@utp.edu.co>
2017-11-08 13:04:04 -0500

    sha1 part 3

--- torrent/sha1.cpp
+++ torrent/sha1.cpp
@@ -1,3 +1,6 @@
+/* This page explain the sha1 algorithm
+  http://www.metamorphosite.com/one-way-hash-encryption-sha1-data-software */
+
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -7,47 +10,57 @@ using namespace std;
 typedef string str;
 
 str strToBin(str _str){
-  /* it will turn a string of alphabet letter in binary form */
+  /* it will turn a string of alphabet letter in 8 bits binary groups for
+  each letter */
   str binary = "";
-  size_t _strSize = _str.size();
-  for(size_t c = 0; c < _strSize; c++){
+  size_t strSize = _str.size();
+  for(size_t c = 0; c < strSize; c++){
     bitset<8> bin(_str.c_str()[c]);
     binary += bin.to_string();
   }
   return binary;
 }
 
-int strOfBinToNum(str _str){
-  /* it will turn a string of binary numbers to decimal form */
+int strBinToNum(str _str){
+  /* it will turn a string of binary numbers to decimal value */
   int decimal = 0;
   for(int i = _str.size()-1, exponent = 0; i>= 0; i--, exponent++)
     if(_str[i] == '1') decimal += pow(2,exponent);
   return decimal;
 }
 
-void fillWithZeros(str& idBin, uint zeroes){
+void fillWithZeroes(str& strBin, uint zeroes){
   /* it will fill with zeroes */
-  for(uint i=0; i<zeroes; i++) idBin.push_back('0');
+  for(uint i=0; i<zeroes; i++) strBin.push_back('0');
 }
 
-uint sha1(str id){
-  /* it will calculate an unique key for id string */
+uint sha1(str _str){
+  /* it will calculate an unique key for _str */
   int h0 = 0b01100111010001010010001100000001,\
       h1 = 0b11101111110011011010101110001001,\
       h2 = 0b10011000101110101101110011111110,\
       h3 = 0b00010000001100100101010001110110,\
       h4 = 0b11000011110100101110000111110000;
 
-  str idBin = strToBin(id);
-  str IdBinCpy = idBin;
-  idBin.push_back('1');
-  int zeroes = 0, bitsLength = idBin.size();
+  // ------------- padding section -------------
+  // we need to build a 512 bits package, this section will try to build the
+  // first part (448), then complete the leftover part (448 + 64 = 512)
+
+  // building the first part (448 bits)
+  str strBin = strToBin(_str); // example -> this will turn "hull" word to \
+                              "01101000 01110101 01101100 01101100"
+  strBin.push_back('1');      // padding bit
+  int zeroes = 0, bitsLength = strBin.size();
   if(bitsLength % 512 <= 448)  zeroes = 448 - (bitsLength % 512);
   else zeroes = 448 + 512 - (bitsLength % 512);
-  fillWithZeros(idBin,zeroes);
-  cout << idBin << endl;
-  //int t = h0 & h1;
-  //cout << "h0 and h1 = "<< t;
+  fillWithZeroes(strBin,zeroes);
+
+  // building the second part (64 bits)
+  size_t strSize = _str.size();
+  zeroes = 64 - sizeof(strSize)*8;
+  fillWithZeroes(strBin,zeroes);
+  strBin += bitset<sizeof(strSize)*8>(strSize*8).to_string();
+  cout << strBin.size() <<endl;
 }
 
 
