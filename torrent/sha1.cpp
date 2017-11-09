6683b6510e63c8b3bff2c61f80dc348f4ec315cd
cjgc <chjugomez@utp.edu.co>
2017-11-09 15:56:47 -0500

    Sha1 part 7

--- torrent/sha1.cpp
+++ torrent/sha1.cpp
@@ -17,15 +17,15 @@ void leftRotate(str& strBin,uint bits){
   strBin.append(leftmost);
 }
 
-void _not(str& strBin){
+str _not(const str& strBin){
   /* it will negate strBin */
-  str newStrBin = "";
+  str negStrBin = "";
   size_t strBinSize = strBin.size();
   for(uint c = 0; c < strBinSize; c++){
-    if(strBin[c] == '1') newStrBin.push_back('0');
-    else newStrBin.push_back('1');
+    if(strBin[c] == '1') negStrBin.push_back('0');
+    else negStrBin.push_back('1');
   }
-  strBin = newStrBin;
+  return negStrBin;
 }
 
 str strToBin(str _str){
@@ -53,6 +53,31 @@ void fillWithZeroes(str& strBin, uint zeroes){
   for(uint i=0; i<zeroes; i++) strBin.push_back('0');
 }
 
+void funct1(const str& B,const str& C,const str& D,str& F){
+  /* it will set F = (B AND C) OR (!B AND D) */
+  uint and1 = strBinToNum(B) & strBinToNum(C);
+  uint and2 = strBinToNum(_not(B)) & strBinToNum(D);
+  uint or1 = and1 | and2;
+  F = bitset<32>(or1).to_string();
+}
+
+void funct2(const str& B,const str& C,const str& D,str& F){
+  /* it will set F = B XOR C XOR D */
+  uint xor1 = strBinToNum(B) ^ strBinToNum(C);
+  uint xor2 = xor1 ^ strBinToNum(D);
+  F = bitset<32>(xor2).to_string();
+}
+
+void funct3(const str& B,const str& C,const str& D,str& F){
+  /* it will set F = (B AND C) OR (B AND D) OR (C AND D) */
+  uint and1 = strBinToNum(B) & strBinToNum(C);
+  uint and2 = strBinToNum(B) & strBinToNum(D);
+  uint and3 = strBinToNum(C) & strBinToNum(D);
+  uint or1 = and1 | and2;
+  uint or2 = or1 | and3;
+  F = bitset<32>(or2).to_string();
+}
+
 void wordsByChunk(uint start,str& strBin,const str& h0,const str& h1, \
           const str& h2,const str& h3,const str& h4){
   /* it will create 80 words from given chunk (chunks are always 512 bits
@@ -73,9 +98,9 @@ void wordsByChunk(uint start,str& strBin,const str& h0,const str& h1, \
   wordsAmount += 64;
   for(uint i=16; i<wordsAmount; i++){
     // performing xor operations
-    uint xorEd = strBinToNum(words[i-3]) xor strBinToNum(words[i-8]);
-    xorEd = xorEd xor strBinToNum(words[i-14]);
-    xorEd = xorEd xor strBinToNum(words[i-16]);
+    uint xorEd = strBinToNum(words[i-3]) ^ strBinToNum(words[i-8]);
+    xorEd = xorEd ^ strBinToNum(words[i-14]);
+    xorEd = xorEd ^ strBinToNum(words[i-16]);
 
     // carrying leftmost bit to rightmost bit
     str newWord = bitset<32>(xorEd).to_string();
@@ -90,32 +115,44 @@ void wordsByChunk(uint start,str& strBin,const str& h0,const str& h1, \
   str A = h0, B = h1, C = h2, D = h3, E = h4, F = "", K = "";
   for(uint w=0; w<wordsAmount; w++){
     if(w < 20){
-      funct1(); // F = (B AND C) OR (!B AND D)
+      funct1(B,C,D,F); // F = (B AND C) OR (!B AND D)
       K = "01011010100000100111100110011001";
     }
-    else if(w >= 20 && w < 40){
-      funct2(); // F = B XOR C XOR D
+    else if(w < 40){
+      funct2(B,C,D,F); // F = B XOR C XOR D
       K = "01101110110110011110101110100001";
     }
-    else if(w >= 40 && w < 60){
-      funct3(); // F = (B AND C) OR (B AND D) OR (C AND D)
+    else if(w < 60){
+      funct3(B,C,D,F); // F = (B AND C) OR (B AND D) OR (C AND D)
       K = "10001111000110111011110011011100";
     }
     else{
-      funct2(); //F = B XOR C XOR D
+      funct2(B,C,D,F); //F = B XOR C XOR D
       K = "11001010011000101100000111010110";
     }
-    D = C; leftRotate(B,30); C = B; B = A; //A = temp;
+
+    str oldA = A;
+    leftRotate(A,5);
+    ulong temp = strBinToNum(A) + strBinToNum(F) + \
+       strBinToNum(E) + strBinToNum(K) + strBinToNum(words[w]);
+
+    E = D; D = C;
+    leftRotate(B,30);
+    C = B; B = oldA;
+
+    A = bitset<32>(temp).to_string();
+
+    if(w == 79) cout << A <<endl;
   }
 }
 
 uint sha1(str _str){
   /* it will calculate an unique key for _str */
-  str h0 = "01100111010001010010001100000001",\
-      h1 = "11101111110011011010101110001001",\
-      h2 = "10011000101110101101110011111110",\
-      h3 = "00010000001100100101010001110110",\
-      h4 = "11000011110100101110000111110000";
+  str h0 = "01100111010001010010001100000001";
+  str h1 = "11101111110011011010101110001001";
+  str h2 = "10011000101110101101110011111110";
+  str h3 = "00010000001100100101010001110110";
+  str h4 = "11000011110100101110000111110000";
 
   // ------------- padding section -------------
   // we need to build a 512 bits package, this section will try to build the
