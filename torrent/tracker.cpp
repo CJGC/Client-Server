64223266d826affc6684434ff48705e3907ef300
cjgc <chjugomez@utp.edu.co>
2017-11-11 12:38:06 -0500

    Tracker base

--- torrent/tracker.cpp
+++ torrent/tracker.cpp
@@ -1,36 +1,43 @@
 #include <zmqpp/zmqpp.hpp>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+//#include "sha1.hh"
 
 using namespace zmqpp;
 using namespace std;
 typedef string str;
+using vec = vector<str>;
+using _map = map<str,vec>;
+
+class tracker{
+
+  private:
+    _map keys;
+    str id, port, ip, idAfter;
+    bool amIFirst, amILast;
+
+  public:
+    tracker(str ip,str port,str id){
+      this->ip = ip;
+      this->port = port;
+      this->id = id;
+    }
+
+    str getId(){ return id;}
+    str getPort(){ return port;}
+    str getIp(){ return ip;}
+    str getIdAfter(){ return idAfter;}
+    void setIdAfter(str id){ idAfter = id;}
+    void setAmILast(bool value){ amILast = value;}
+    void setAmIFirst(bool value){ amIFirst = value;}
+};
 
 int main(int argc,const char **argv){
     if(argc != 2){
       cerr << "usage "<<argv[0]<<" <string>\n";
       return -1;
     }
-    // cout << sha1(argv[1]) << endl;
-    //  Prepare our context and socket
-    // context ctx;
-    // socket sock(ctx,socket_type::req);
-    //
-    // cout << "Connecting to hello world server…" << std::endl;
-    // sock.connect ("tcp://localhost:5555");
-    //
-    // //  Do 10 requests, waiting each time for a response
-    // for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-    //     message request;
-    //     request << "node 1";
-    //     sock.send (request);
-    //
-    //     //  Get the reply.
-    //     message reply;
-    //     sock.receive(reply);
-    //     str rep;
-    //     reply >> rep;
-    //     cout << "reply " << rep << std::endl;
-    // }
     return 0;
 }
