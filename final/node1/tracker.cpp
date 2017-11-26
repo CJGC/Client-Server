a393fb6bc68b8abbf1b263e71b583bdc05c0a983
cjgc <chjugomez@utp.edu.co>
2017-11-26 02:02:00 -0500

    Code adjustedments were introduced. Connect and disconnect methods were introduced into subscriber section

--- final/node1/tracker.cpp
+++ final/node1/tracker.cpp
@@ -25,7 +25,7 @@ class tracker{
           client and server side respectively --- */
 
   private:
-    _map keys, finTabl;
+    _map keys;
     str id, ip, port, remoteId, remoteIp, remotePort, \
                       befId, befIp, befPort, amILast;
     mutex ubd; // unbind mutex
@@ -93,18 +93,19 @@ class tracker{
       this->amILast = "true";
       this->_exit = false;
       this->mustIUnbindBef = false;
+      this->canIStart = false;
       setKeys(ownFiles);
       buildFinTabl();
     }
 
-    void client(socket& cli,socket& printer,socket& aux){
+    void client(socket& cli,socket& printer, socket& publ){
       /* it will simulate a client into tracker */
       str userOp;
       cout <<"type something to connect";
       getline(cin,userOp);
       bindWithChord(cli);
-      if(this->amILast != "true")
-        setFinTabl(aux);
+      this->canIStart = true;
+      this->sbcv.notify_one();
       /* printing after binding node */
       message request, answer;
       str keys;
@@ -114,63 +115,13 @@ class tracker{
       printer.receive(answer);
       cout << "type something to exit: ";
       getline(cin,userOp);
+      publisher(publ);
       if(this->_exit)
         return;
       unbindWithChord(cli);
     }
 
   private:
-    void buildFinTabl(){
-      /* it will build finger table */
-      uint finTablDepth = log2( (double)(sizeof(uint) * 8 ) );
-      for(uint i = 0; i<finTablDepth; i++){
-        str key = this->id + to_string( pow(2,i) );
-        key = sha1(key);
-        vec second;
-        second.resize(2);
-        second[0] = "";
-        second[1] = "";
-        this->finTabl[key] = second;
-      }
-    }
-
-    void searchIntoRing(socket& aux,_map::iterator& item){
-      /* it will search into chord ring for binding with */
-      str key = item->first, ip = this->remoteIp, port = this->remotePort;
-
-      while(true){
-        message request, answer;
-        aux.connect("tcp://"+ip+":"+port);
-        request << "getinfo" << " " << " " << " " << " " << " ";
-        aux.send(request);
-        aux.receive(answer);
-        str remtId = "", remtNextId = "", remtNextIp = "", remtNextPort = "",\
-            remtIsLast = "";
-        answer >> remtId >> remtNextId >> remtNextIp >> remtNextPort \
-               >> remtIsLast;
-
-        if(key <= remtId || remtIsLast == "true"){
-          item->second[0] = ip;
-          item->second[1] = port;
-          break;
-        }
-
-        aux.disconnect("tcp://"+ip+":"+port);
-        ip = remtNextIp;
-        port = remtNextPort;
-      }
-
-      aux.disconnect("tcp://"+ip+":"+port);
-    }
-
-    void setFinTabl(socket& aux){
-      /* it will set up finger table */
-      _map::iterator start = this->finTabl.begin();
-      _map::iterator end = this->finTabl.end();
-      for(auto& it = start; it != end; it++){
-        searchIntoRing(aux,it);
-      }
-    }
 
     void unbindWithChord(socket& cli){
       /* it will unbind with chord ring */
@@ -383,6 +334,101 @@ class tracker{
       this->_exit = true;
       package << "ok";
     }
+
+    /* --------------------- publisher side ----------------- */
+  private:
+    void publisher(socket& publ){
+      /* it will publish to subscriber when this client disconnect */
+      message request;
+      request << "disconnect";
+      publ.send(request);
+    }
+
+    /* --------------------- subscriber side ----------------- */
+  private:
+    mutex sbm; // subscriber mutex
+    condition_variable sbcv; // subscriber condition variable
+    _map finTabl;
+    bool canIStart;
+
+  public:
+    void subscriber(socket& subs,socket& aux){
+      /* main subscriber section */
+      unique_lock<mutex> lock(this->sbm);
+      this->sbcv.wait(lock,[&]{return this->canIStart;});
+      if(this->amILast != "true")
+        setFinTabl(aux);
+      conWithAllPubl(subs);
+      disconWithAllPubl(subs);
+    }
+
+  private:
+    void buildFinTabl(){
+      /* it will build finger table */
+      uint finTablDepth = log2( (double)(sizeof(uint) * 8 ) );
+      for(uint i = 0; i<finTablDepth; i++){
+        str key = this->id + to_string( pow(2,i) );
+        key = sha1(key);
+        vec second;
+        second.resize(2);
+        second[0] = "";
+        second[1] = "";
+        this->finTabl[key] = second;
+      }
+    }
+
+    void searchIntoRing(socket& aux,_map::iterator& item){
+      /* it will search into chord ring for binding with */
+      str key = item->first, ip = this->remoteIp, port = this->remotePort;
+
+      while(true){
+        message request, answer;
+        aux.connect("tcp://"+ip+":"+port);
+        request << "getinfo" << " " << " " << " " << " " << " ";
+        aux.send(request);
+        aux.receive(answer);
+        str remtId = "", remtNextId = "", remtNextIp = "", remtNextPort = "",\
+            remtIsLast = "";
+        answer >> remtId >> remtNextId >> remtNextIp >> remtNextPort \
+               >> remtIsLast;
+
+        if(key <= remtId || remtIsLast == "true"){
+          item->second[0] = ip;
+          item->second[1] = port;
+          break;
+        }
+
+        aux.disconnect("tcp://"+ip+":"+port);
+        ip = remtNextIp;
+        port = remtNextPort;
+      }
+
+      aux.disconnect("tcp://"+ip+":"+port);
+    }
+
+    void setFinTabl(socket& aux){
+      /* it will set up finger table */
+      _map::iterator start = this->finTabl.begin();
+      _map::iterator end = this->finTabl.end();
+      for(auto& it = start; it != end; it++)
+        searchIntoRing(aux,it);
+    }
+
+    void conWithAllPubl(socket& subs){
+      /* it will connect with all sockets into finger table */
+      for(auto& item : this->finTabl){
+        str ip = item.second[0], port = item.second[1];
+        subs.connect("tcp://"+ip+":"+port);
+      }
+    }
+
+    void disconWithAllPubl(socket& subs){
+      /* it will disconnect with all sockets into finger table*/
+      for(auto& item : this->finTabl){
+        str ip = item.second[0], port = item.second[1];
+        subs.disconnect("tcp://"+ip+":"+port);
+      }
+    }
 };
 
 int main(int argc,const char **argv){
@@ -390,22 +436,27 @@ int main(int argc,const char **argv){
   map<str,str> machInfo = getMachInfo();
   ownFiles = getFiles(machInfo["ip"],"/files");
   id = sha1(machInfo["mac"]+"1");
-  context s_ctx, c_ctx, p_ctx, a_ctx;
+  context s_ctx, c_ctx, p_ctx, a_ctx, su_ctx, pu_ctx;
   socket serv(s_ctx,socket_type::rep), cli(c_ctx,socket_type::req)\
-         ,printer(p_ctx,socket_type::req), aux(a_ctx,socket_type::req);
+        ,printer(p_ctx,socket_type::req), aux(a_ctx,socket_type::req)\
+        ,subs(su_ctx,socket_type::subscribe), publ(pu_ctx,socket_type::publish);
+  subs.subscribe(""); // default channel
+  publ.bind("tcp://"+localIp+":5556"); // binding publisher
   /* printing current keys domain */
-  message request, answer;
-  printer.connect("tcp://"+printerIp+":7777");
-  request << id << ownFiles;
-  printer.send(request);
-  printer.receive(answer);
+  // message request, answer;
+  // printer.connect("tcp://"+printerIp+":7777");
+  // request << id << ownFiles;
+  // printer.send(request);
+  // printer.receive(answer);
   /* incoming node into chord ring */
-  tracker track(id,localIp,"5555",remoteIp,"5556",ownFiles);
-  thread t0(&tracker::_unbindBef, &track, ref(cli));
-  thread t1(&tracker::server, &track, ref(serv));
-  track.client(cli,printer,aux);
+  tracker track(id,localIp,"5555",remoteIp,"5557",ownFiles);
+  thread t0(&tracker::_unbindBef, &track, ref(cli)),\
+         t1(&tracker::server, &track, ref(serv)),\
+         t2(&tracker::subscriber, &track, ref(subs), ref(aux));
+  track.client(cli,printer,publ);
   t0.join();
   t1.join();
-  printer.disconnect("tcp://"+printerIp+":7777");
+  t2.join();
+  // printer.disconnect("tcp://"+printerIp+":7777");
   return 0;
 }
